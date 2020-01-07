c02ac4a17c6a06621ab83c461baa5acd1b4f6923
Victor Batalha <madivcb@gmail.com>
2020-01-07 16:24:38 -0300

    exclude password and password_hash from return

--- server/src/app/controllers/UserController.js
+++ server/src/app/controllers/UserController.js
@@ -5,6 +5,8 @@ class UserController {
     try {
       const user = await User.create(req.body);
 
+      user.excludePasswordFromReturn();
+
       return res.status(201).json(user);
     } catch (error) {
       return res.status(400).json(error.message);
