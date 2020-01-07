3f9fd65524b7b2772d3871a2f2352f0a9b8453eb
Victor Batalha <madivcb@gmail.com>
2020-01-07 15:25:49 -0300

    user register route

---
+++ server/src/app/controllers/UserController.js
@@ -0,0 +1,15 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const user = await User.create(req.body);
+
+      return res.status(201).json(user);
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
+  }
+}
+
+export default new UserController();
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import { UserController } from './app/controllers';
+
 const routes = Router();
 
-routes.get('/', (req, res) => res.send('Hello World!'));
+routes.post('/users', UserController.store);
 
 export default routes;
